1fc98ee856c23a28b837a9ce9beee137e284ae55
Wyatt Alt <wyatt.alt@gmail.com>
2019-01-03 19:19:03 -0800

    first commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject streaming-demo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :jvm-opts ["-Xmx64m"]
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core "1.7.1"]
+                 [org.clojure/core.async "0.4.490"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler streaming-demo.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
---
+++ src/streaming_demo/handler.clj
@@ -0,0 +1,36 @@
+(ns streaming-demo.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [ring.util.response :as rr]
+            [ring.util.io :refer [piped-input-stream]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:import [java.io IOException]))
+
+(defn rand-str []
+  (apply str (take 10 (repeatedly #(char (+ (rand 26) 65))))))
+
+(defn rand-value []
+  {:foo (rand-str) :bar (rand-str) :baz (rand-str)})
+
+(defroutes app-routes
+  (GET "/stream" []
+       (let [result-seq (take 10000000 (repeatedly rand-value))]
+         (rr/response
+           (piped-input-stream
+             #(let [w (io/make-writer % {:encoding "UTF-8"})]
+                (json/generate-stream result-seq w {:pretty true}))))))
+  (GET "/nostream" []
+       (let [result-seq (take 10000000 (repeatedly rand-value))]
+         (rr/response
+           (piped-input-stream
+             #(let [w (io/make-writer % {:encoding "UTF-8"})]
+                (try
+                  (json/generate-stream result-seq w {:pretty true})
+                  (catch Exception e
+                    (println "yo"))))))))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
